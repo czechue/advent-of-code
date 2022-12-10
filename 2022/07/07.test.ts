2a8f5706c78c62d6d56a4378340ecdc0d5d31f53
Michal <michal.lester@fourthwall.com>
2022-12-11 00:33:44 +0100

    2022-07

---
+++ 2022/07/07.test.ts
@@ -0,0 +1,73 @@
+import { input } from './input';
+import { findTheSmallestDirectory, funA, getSumOfAtMost100000 } from './07';
+
+describe('07', () => {
+  test('check root children (1st level)', () => {
+    const result = funA(input);
+    expect(result.root.children).toHaveLength(8);
+    expect(result.root.children[0].data.name).toBe('fts');
+    expect(result.root.children[6].data.name).toBe('zbdp.gqb');
+    expect(result.root.children[6].data.size).toBe(195891);
+  });
+
+  test('check "fts" folder children (2st level)', () => {
+    const ftsFolder = funA(input).root.children[0];
+    expect(ftsFolder.data.name).toBe('fts');
+    expect(ftsFolder.children).toHaveLength(3);
+    expect(ftsFolder.children[0].data.name).toBe('dlqtffw');
+    expect(ftsFolder.children[2].data.name).toBe('wvwhrb.dhh');
+    expect(ftsFolder.children[2].data.size).toBe(254713);
+  });
+
+  test('check "/fts/dlqtffw" folder children (3st level)', () => {
+    const dlqtffwFolder = funA(input).root.children[0].children[0];
+    expect(dlqtffwFolder.data.name).toBe('dlqtffw');
+    expect(dlqtffwFolder.children).toHaveLength(1);
+    expect(dlqtffwFolder.children[0].data.name).toBe('nqbvg.fgd');
+    expect(dlqtffwFolder.children[0].data.size).toBe(73533);
+  });
+
+  test('check if cd.. works by reading "/fts/rbfmmjvd" folder children (3st level)', () => {
+    const dlqtffwFolder = funA(input).root.children[0].children[1];
+    expect(dlqtffwFolder.data.name).toBe('rbfmmjvd');
+    expect(dlqtffwFolder.children).toHaveLength(1);
+    expect(dlqtffwFolder.children[0].data.name).toBe('zcgrgff.fnf');
+    expect(dlqtffwFolder.children[0].data.size).toBe(290697);
+  });
+
+  test('check if cd.. cd.. works by reading "/jnwr/" folder children (2st level)', () => {
+    const dlqtffwFolder = funA(input).root.children[1];
+    expect(dlqtffwFolder.data.name).toBe('jnwr');
+    expect(dlqtffwFolder.children).toHaveLength(4);
+    expect(dlqtffwFolder.children[0].data.name).toBe('ghmtnzr');
+    expect(dlqtffwFolder.children[0].data.size).toBe(323577);
+    expect(dlqtffwFolder.children[3].data.name).toBe('zzbvdcf');
+  });
+
+  // test('set correctly closest parent directory current directory', () => {
+  //   const example = '$ cd /\n$ ls\ndir fts\ndir jnwr';
+  // });
+
+  test('all childrens sizes of "zddrb" are correctly summed up', () => {
+    const zddrbFolder = funA(input).root.children[7];
+    const childrenSum = zddrbFolder.children.reduce(
+      (acc, curr) => acc + curr.data.size,
+      0
+    );
+    expect(zddrbFolder.data.size).toBe(childrenSum);
+  });
+
+  test('input', () => {
+    expect(funA(input)).toBe(1);
+  });
+
+  test('getSumOfAtMost100000', () => {
+    const tree = funA(input);
+    expect(getSumOfAtMost100000(tree)).toBe(1642503);
+  });
+
+  test('findTheSmallestDirectory', () => {
+    const tree = funA(input);
+    expect(findTheSmallestDirectory(tree)).toBe(6999588);
+  });
+});
---
+++ 2022/07/07.ts
@@ -0,0 +1,159 @@
+const parseStringToArr = (input: string): any[] => {
+  return input.split('\n');
+};
+
+type Data = {
+  name: string;
+  size: number | undefined;
+  type?: 'directory' | 'file';
+};
+
+class Node {
+  data: Data;
+  children: Node[];
+
+  constructor(data: Data) {
+    this.data = data;
+    this.children = [];
+  }
+
+  add(data: Data) {
+    const node = new Node(data);
+    this.children.push(node);
+  }
+
+  remove(name: Data['name']) {
+    this.children = this.children.filter((node) => {
+      return node.data.name !== name;
+    });
+  }
+}
+
+class Tree {
+  root: Node;
+
+  constructor() {
+    this.root = null;
+  }
+
+  traverseBF(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+
+      arr.push(...node.children);
+      fn(node);
+    }
+  }
+
+  traverseDF(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+
+      arr.unshift(...node.children);
+      fn(node);
+    }
+  }
+
+  findClosestParent(node: Node): Node {
+    let closestParent = null;
+    this.traverseBF((currentNode) => {
+      if (currentNode.children.includes(node)) {
+        closestParent = currentNode;
+      }
+    });
+    return closestParent;
+  }
+}
+
+function recursiveSum(tree: Tree, currentDirectory: Node) {
+  if (currentDirectory.children.every((node) => node.data.size)) {
+    const size = currentDirectory.children.reduce((acc, node) => {
+      return acc + node.data.size;
+    }, 0);
+    currentDirectory.data.size = size;
+    // here we know that currentDirectory is a directory
+    currentDirectory.data.type = 'directory';
+    const closestParent = tree.findClosestParent(currentDirectory);
+
+    if (closestParent) {
+      recursiveSum(tree, closestParent);
+    }
+  }
+}
+
+export const funA = (input: string) => {
+  const arr = parseStringToArr(input).splice(1);
+
+  const tree = new Tree();
+  tree.root = new Node({ name: '/', size: undefined });
+
+  let currentDirectory = tree.root;
+
+  for (const command of arr) {
+    console.dir(command);
+    if (command.startsWith('$ ls')) {
+      console.log('$ ls');
+    } else if (command.startsWith('$ cd')) {
+      const [, newDirectory] = command.split('cd ');
+
+      if (newDirectory === '..') {
+        currentDirectory = tree.findClosestParent(currentDirectory);
+      } else {
+        const child = currentDirectory.children.find(
+          (node) => node.data.name === newDirectory
+        );
+
+        if (child) {
+          currentDirectory = child;
+        }
+      }
+    } else {
+      // dir abc
+      if (command.startsWith('dir')) {
+        const [, folderName] = command.split('dir ');
+        currentDirectory.add({ name: folderName, size: undefined });
+      } else {
+        // 128 d.log
+        const [size, fileName] = command.split(' ');
+        currentDirectory.add({ name: fileName, size: parseInt(size) });
+      }
+    }
+
+    // if all children are files, then return the sum of their sizes and set size of folder to sum of files size
+    // and make this function recursive for closest parent
+
+    recursiveSum(tree, currentDirectory);
+  }
+
+  return tree;
+};
+
+export const getSumOfAtMost100000 = (tree: Tree) => {
+  const counters = [];
+  tree.traverseBF((node) => {
+    if (node.data.type === 'directory' && node.data.size < 100000) {
+      counters.push(node.data.size);
+    }
+  });
+
+  return counters.reduce((acc, size) => acc + size, 0);
+};
+
+const totalFilesystemDiskSpace = 70000000;
+const neededUnusedSpace = 30000000;
+
+export const findTheSmallestDirectory = (tree: Tree) => {
+  const currentUnusedSpace = totalFilesystemDiskSpace - tree.root.data.size;
+  const spaceNeeded = neededUnusedSpace - currentUnusedSpace;
+
+  const allDirectories = [];
+  tree.traverseBF((node) => {
+    if (node.data.type === 'directory' && node.data.size > spaceNeeded) {
+      allDirectories.push(node.data.size);
+    }
+  });
+
+  return allDirectories.sort((a, b) => a - b)[0];
+};
